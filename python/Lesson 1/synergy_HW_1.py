d35d4c156d312044f1c9e57df2cbac1eac38c7c1
dkhurich <dhuric231@gmail.com>
2024-08-21 20:16:46 +0300

    complited HW 8

--- python/Lesson 1/synergy_HW_1.py
+++ python/Lesson 1/synergy_HW_1.py
@@ -12,14 +12,27 @@ def get_year_suffix(age_str):
     else:
         return "лет"
 
+pets = {}
+
 # Ввод данных
+print("Введите имя питомца:")
+pet_name = input()
 print("Введите вид питомца:")
 pet_type = input()
 print("Введите возраст питомца:")
-pet_age = input()
-print("Введите имя питомца:")
-pet_name = input()
+pet_age = int(input())
+print("Введите имя хозяина:")
+pet_owner = input()
+
+pets = {
+    pet_name: {
+        'pet_type' : pet_type,
+        'pet_age' : pet_age,
+        'pet_owner' : pet_owner
+    }
+}
 
 # Получаем окончание для возраста
-year_suffix = get_year_suffix(pet_age)
-print(f"Это {pet_type} по кличке \"{pet_name}\". Возраст: {pet_age} {year_suffix}.")
+year_suffix = get_year_suffix(pets[pet_name]['pet_age'])
+# Форматируем вывод
+print(f"Это {pets[pet_name]['pet_type']} по кличке \"{pet_name}\". Возраст: {pets[pet_name]['pet_age']} {year_suffix}. Владелец: {pets[pet_name]['pet_owner']}")
---
+++ python/Lesson 8/synergy_HW_1.py
@@ -0,0 +1,8 @@
+my_dict = {}
+
+# Создаем словарь с ключами от 10 до -5
+for num in range(10, -6, -1):
+    my_dict[num] = num ** num
+
+# Печатаем результат
+print(my_dict)
